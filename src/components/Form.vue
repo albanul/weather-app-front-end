9eab40bc2b5aab54679e43d798a3455a814f8820
Alex Banul <albanul92@gmail.com>
2021-05-09 01:57:11 +0200

    Implement fetching forecast by zip code

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -4,49 +4,95 @@
       ref="form"
       v-model="valid"
       lazy-validation>
-    <v-text-field
-        v-model="cityName"
-        :rules="cityNameRules"
-        autocomplete="false"
-        label="City name"
-        required
-    ></v-text-field>
 
-    <v-btn
-        :disabled="!valid"
-        color="success"
-        class="mr-4"
-        type="submit"
-        :loading="loading"
-    >
-      Check weather
-    </v-btn>
+    <v-row>
+      <v-col
+          class="d-flex"
+          cols="12"
+          sm="6"
+      >
+        <v-text-field
+            v-model="searchQuery"
+            :rules="searchQueryRules"
+            autocomplete="false"
+            :label="searchType"
+            required
+        ></v-text-field>
+      </v-col>
+      <v-col
+          class="d-flex"
+          cols="12"
+          sm="6"
+      >
+        <v-select
+            v-model="searchType"
+            v-on:change="onSearchTypeChange"
+            :items="searchTypes"
+            label="Search type"
+        ></v-select>
+      </v-col>
+    </v-row>
+
+    <v-row justify="center">
+      <v-btn
+          :disabled="!valid"
+          color="success"
+          class="mr-4"
+          type="submit"
+          :loading="loading"
+      >
+        Get forecast
+      </v-btn>
+    </v-row>
   </v-form>
 </template>
 
 <script>
 import forecastService from "@/services/forecastService";
 
+const cityName = 'City name';
+const zipCode = 'ZIP code';
+
+const rulesDictionary = {};
+rulesDictionary[cityName] = v => !!v || 'Name is required';
+rulesDictionary[zipCode] = v => {
+  const pattern = /(?!01000|99999)(0[1-9]\d{3}|[1-9]\d{4})/
+  return pattern.test(v) || 'Invalid ZIP code'
+}
+
+const getForecastDictionary = {};
+getForecastDictionary[cityName] = async cityName => await forecastService.getForecastByCityName(cityName);
+getForecastDictionary[zipCode] = async zipCode => await forecastService.getForecastByZipCode(zipCode);
+
 export default {
   name: 'Form',
   data: () => ({
     loading: false,
     valid: false,
-    cityName: '',
-    cityNameRules: [
-      v => !!v || 'Name is required',
-    ],
+    searchQuery: '',
+    searchType: '',
+    searchTypes: [cityName, zipCode]
   }),
+  created() {
+    this.searchType = this.searchTypes[0];
+  },
   methods: {
     onSubmit: async function () {
       if (this.$refs.form.validate()) {
         this.loading = true;
-        const forecast = await forecastService.getForecastByCityName(this.cityName);
+        const forecast = await getForecastDictionary[this.searchType](this.searchQuery);
         this.loading = false;
-        console.dir(forecast);
         this.$emit('forecast-received', forecast)
       }
+    },
+    onSearchTypeChange: function () {
+      this.searchQuery = '';
     }
+  },
+  computed: {
+    searchQueryRules: function () {
+      return [v => rulesDictionary[this.searchType](v)]
+    },
   }
 }
 </script>
